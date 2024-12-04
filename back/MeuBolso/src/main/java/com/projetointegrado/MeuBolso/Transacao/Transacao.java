38dff356361023be6f4feec73321d34cbc74eaa4
Lucas Ferreira <lukasnoobre53@gmail.com>
2024-12-04 13:33:23 -0300

    feat: entity transacao

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/Transacao/Transacao.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/Transacao/Transacao.java
@@ -1,8 +1,35 @@
-package com.projetointegrado.MeuBolso.Receita;
+package com.projetointegrado.MeuBolso.Transacao;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
-public class Receita {
+public class Transacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private BigDecimal valor;
 
+    private Boolean e_fixo;
+
+    private Integer qtd_repeticao;
+
+    @Column(columnDefinition = "TEXT")
+    private String comentario;
+
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    private Date data_transacao;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getId() {
+        return id;
+    }
 }
