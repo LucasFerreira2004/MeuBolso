04fb2cfdc8e812148e7061589deab79e90c97459
Lucas Ferreira <lukasnoobre53@gmail.com>
2024-12-05 19:22:12 -0300

    back-feat: seed tipoConta

--- back/MeuBolso/src/main/resources/import.sql
+++ back/MeuBolso/src/main/resources/import.sql
@@ -1,3 +1,11 @@
+/*TIPO CONTA*/
+insert into tipo_conta(tipo_conta) values ('conta_corrente');
+insert into tipo_conta(tipo_conta) values ('carteira');
+insert into tipo_conta(tipo_conta) values ('poupanca');
+insert into tipo_conta(tipo_conta) values ('investimentos');
+insert into tipo_conta(tipo_conta) values ('outro');
+
+/*TRANSACAO*/
 insert into transacao (valor, data_transacao, descricao, tipo) values ('1000', '2024-12-01', 'venda bicicleta', 'receita');
 insert into transacao (valor, data_transacao, descricao, tipo) values ('16.28', '2024-12-01', 'açaí', 'despesa');
 insert into transacao (valor, data_transacao, descricao, tipo) values ('1412', '2024-12-05', 'salario', 'receita');
