7753ce5ba7a3f25858ceb31f5e40347e4fc76c0c
Miqueias Bento <miqueias.bento32@gmail.com>
2024-12-05 22:16:11 -0300

    back-feat: seed categorias

--- back/MeuBolso/src/main/resources/import.sql
+++ back/MeuBolso/src/main/resources/import.sql
@@ -16,3 +16,20 @@ insert into transacao (valor, data_transacao, descricao, tipo, conta_origem) val
 insert into transacao (valor, data_transacao, descricao, tipo, conta_origem) values ('1412', '2024-12-05', 'salario', 'receita', '2');
 insert into transacao (valor, data_transacao, descricao, tipo, conta_origem) values ('123.55', '2024-12-10', 'conta luz', 'despesa', '1');
 insert into transacao (valor, data_transacao, descricao, tipo, conta_origem) values ('76.20', '2024-12-11', 'conta água', 'despesa', '1');
+
+
+/*CATEGORIAS*/
+insert into categoria (nome, tipo_categoria, cor) values ('casa', 'despesa', '8755BC');
+insert into categoria (nome, tipo_categoria, cor) values ('alimentacao', 'despesa', 'BA0707');
+insert into categoria (nome, tipo_categoria, cor) values ('transporte', 'despesa', 'C19F13');
+insert into categoria (nome, tipo_categoria, cor) values ('educacao', 'despesa', '33A841');
+insert into categoria (nome, tipo_categoria, cor) values ('lazer', 'despesa', '57BEA1');
+insert into categoria (nome, tipo_categoria, cor) values ('roupas', 'despesa', '5797BE');
+insert into categoria (nome, tipo_categoria, cor) values ('saude', 'despesa', 'C92D78');
+insert into categoria (nome, tipo_categoria, cor) values ('pagamentos', 'despesa', '818181');
+
+insert into categoria (nome, tipo_categoria, cor) values ('salario', 'receita', '33A841');
+insert into categoria (nome, tipo_categoria, cor) values ('investimentos', 'receita', '0775BA');
+insert into categoria (nome, tipo_categoria, cor) values ('presente', 'receita', '8755BC');
+insert into categoria (nome, tipo_categoria, cor) values ('renda extra', 'receita', 'CE601B');
+insert into categoria (nome, tipo_categoria, cor) values ('outros', 'receita', 'BC7D18');
