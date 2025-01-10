bbe671b52a838c0c56995516c728b594bf3df541
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-01-10 19:00:06 -0300

    back-feat: Configuracao de Injecoes e refac de exceptions categoria

---
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/config/InjecaoDependeciaConfig.java
@@ -0,0 +1,56 @@
+package com.projetointegrado.MeuBolso.config;
+
+import com.projetointegrado.MeuBolso.autorizacao.token.ITokenService;
+import com.projetointegrado.MeuBolso.autorizacao.token.TokenService;
+import com.projetointegrado.MeuBolso.banco.BancoService;
+import com.projetointegrado.MeuBolso.banco.IBancoService;
+import com.projetointegrado.MeuBolso.categoria.CategoriaService;
+import com.projetointegrado.MeuBolso.categoria.ICategoriaService;
+import com.projetointegrado.MeuBolso.conta.ContaService;
+import com.projetointegrado.MeuBolso.conta.IContaService;
+import com.projetointegrado.MeuBolso.tipoConta.ITipoContaService;
+import com.projetointegrado.MeuBolso.tipoConta.TipoContaService;
+import com.projetointegrado.MeuBolso.transacao.ITransacaoService;
+import com.projetointegrado.MeuBolso.transacao.TransacaoService;
+import com.projetointegrado.MeuBolso.usuario.IUsuarioService;
+import com.projetointegrado.MeuBolso.usuario.UsuarioService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InjecaoDependeciaConfig {
+    @Bean
+    public IBancoService BancoService() {
+        return new BancoService();
+    }
+
+    @Bean
+    public ITokenService TokenService() {
+        return new TokenService();
+    }
+
+    @Bean
+    public ICategoriaService CategoriaService() {
+        return new CategoriaService();
+    }
+
+    @Bean
+    public IContaService ContaService() {
+        return new ContaService();
+    }
+
+    @Bean
+    public ITipoContaService TipoContaService() {
+        return new TipoContaService();
+    }
+
+    @Bean
+    public ITransacaoService TransacaoService() {
+        return new TransacaoService();
+    }
+    
+    @Bean
+    public IUsuarioService UsuarioService() {
+        return new UsuarioService();
+    }
+}
