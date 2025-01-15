2478d534761e5b5355624c6d4a1eda7061d79cbf
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-01-14 22:43:08 -0300

    back-feat: verificando campos null em transacaoDTO

---
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/globalExceptions/ValorNullRecebidoException.java
@@ -0,0 +1,7 @@
+package com.projetointegrado.MeuBolso.globalExceptions;
+
+public class ValorNullRecebidoException extends RuntimeException {
+  public ValorNullRecebidoException(String message) {
+    super(message);
+  }
+}
