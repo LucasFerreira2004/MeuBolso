7979d9b0d03ec8038bb528e4fba88ff08728344f
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-01-14 23:08:04 -0300

    back-add: excceptionHandler para erros nos @valids

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/globalExceptions/ValorNullRecebidoException.java
+++
@@ -1,7 +0,0 @@
-package com.projetointegrado.MeuBolso.globalExceptions;
-
-public class ValorNullRecebidoException extends RuntimeException {
-  public ValorNullRecebidoException(String message) {
-    super(message);
-  }
-}
