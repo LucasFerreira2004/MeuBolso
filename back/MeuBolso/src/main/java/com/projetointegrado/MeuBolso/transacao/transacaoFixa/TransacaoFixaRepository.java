324f43b47f902825d952deca16309137a9272b31
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-01-17 20:53:19 -0300

    back-add: add transacaoFixaDTO

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/transacao/transacaoFixa/TransacaoFixaRepository.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/transacao/transacaoFixa/TransacaoFixaRepository.java
@@ -1,6 +1,13 @@
 package com.projetointegrado.MeuBolso.transacao.transacaoFixa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TransacaoFixaRepository extends JpaRepository<TransacaoFixa, Long> {
+    @Query(nativeQuery = true, value = """
+        select * from transacaoFixa where usuario_id = :userId;
+    """)
+    public List<TransacaoFixa> findAllByUsuario(String userId);
 }
--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/transacao/transacaoFixa/TransacaoFixaService.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/transacao/transacaoFixa/TransacaoFixaService.java
@@ -1,12 +1,19 @@
 package com.projetointegrado.MeuBolso.transacao.transacaoFixa;
 
+import com.projetointegrado.MeuBolso.transacao.transacaoFixa.dto.TransacaoFixaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class TransacaoFixaService implements ITransacaoFixaService {
     @Autowired
     private TransacaoFixaRepository transacaoFixaRepository;
 
-
+    @Transactional
+    public List<TransacaoFixaDTO> findAll(String userId){
+        return transacaoFixaRepository.findAllByUsuario(userId).stream().map(x -> new TransacaoFixaDTO(x));
+    }
 }
