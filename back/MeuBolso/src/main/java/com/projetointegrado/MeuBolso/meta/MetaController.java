e486b506c7eea149aaa6b671ffde027e2b2a87c3
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2025-01-18 15:30:11 -0300

    back-feat: tratando rotas com excecoes personalizadas de metas

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/meta/MetaController.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/meta/MetaController.java
@@ -1,6 +1,9 @@
 package com.projetointegrado.MeuBolso.meta;
 
+import com.projetointegrado.MeuBolso.meta.dto.MetaDTO;
+import com.projetointegrado.MeuBolso.meta.dto.MetaPostDTO;
 import com.projetointegrado.MeuBolso.usuario.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -20,27 +23,32 @@ public class MetaController {
     private UsuarioService usuarioService;
 
     @GetMapping
-    public List<Meta> findAll() {
-        return metaService.findAll(usuarioService.getUsuarioLogadoId());
+    public List<MetaDTO> findAll() {
+        String usuarioId = usuarioService.getUsuarioLogadoId();
+        return metaService.findAll(usuarioId);
     }
 
-    @GetMapping("/{id}")
-    public Meta findById(@PathVariable Long id) {
-        return metaService.findById(usuarioService.getUsuarioLogadoId(), id).get();
+    @GetMapping("/{metaId}")
+    public MetaDTO findById(@PathVariable Long metaId) {
+        String usuarioId = usuarioService.getUsuarioLogadoId();
+        return metaService.findById(usuarioId, metaId);
     }
 
     @PostMapping
-    public Meta save(@RequestBody Meta meta) {
-        return metaService.save(meta);
+    public MetaDTO save(@RequestBody MetaPostDTO meta) {
+        String usuarioId = usuarioService.getUsuarioLogadoId();
+        return metaService.save(usuarioId, meta);
     }
 
-    @PutMapping
-    public Meta update(@RequestBody Meta meta) {
-        return metaService.update(meta);
+    @PutMapping("/{metaId}")
+    public MetaDTO update(@PathVariable Long metaId, @RequestBody MetaPostDTO meta) {
+        String usuarioId = usuarioService.getUsuarioLogadoId();
+        return metaService.update(usuarioId, metaId, meta);
     }
 
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        metaService.delete(usuarioService.getUsuarioLogadoId(), usuarioService.getUsuarioLogadoId());
+    @DeleteMapping("/{metaId}")
+    public void delete(@PathVariable Long metaId) {
+        String usuarioId = usuarioService.getUsuarioLogadoId();
+        metaService.delete(usuarioId, metaId);
     }
 }
--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/meta/MetaService.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/meta/MetaService.java
@@ -1,13 +1,17 @@
 package com.projetointegrado.MeuBolso.meta;
 
+import com.projetointegrado.MeuBolso.globalExceptions.AcessoNegadoException;
+import com.projetointegrado.MeuBolso.meta.dto.MetaDTO;
+import com.projetointegrado.MeuBolso.meta.dto.MetaPostDTO;
+import com.projetointegrado.MeuBolso.meta.exception.MetaNaoEncontradaException;
 import com.projetointegrado.MeuBolso.usuario.Usuario;
 import com.projetointegrado.MeuBolso.usuario.UsuarioRepository;
+import com.projetointegrado.MeuBolso.usuario.exception.UsuarioNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class MetaService implements IMetaService {
@@ -17,31 +21,54 @@ public class MetaService implements IMetaService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-
     @Transactional(readOnly = true)
-    public List<Meta> findAll(String usuarioId) {
-        Usuario usuario = usuarioRepository.findById(usuarioId).get();
-        return metaRepository.findAllByUsuario(usuarioId);              // Retornar DTO
+    public List<MetaDTO> findAll(String usuarioId) {
+        List<Meta> result = metaRepository.findAllByUsuario(usuarioId);
+
+        return result.stream().map(MetaDTO::new).toList();
     }
 
     @Transactional(readOnly = true)
-    public Optional<Meta> findById(String usuarioId, Long metaId) {
-        Usuario usuario = usuarioRepository.findById(usuarioId).get();
-        return metaRepository.findById(metaId);              // Fazer tratamento de exceções
+    public MetaDTO findById(String usuarioId, Long metaId) {
+        Meta result = metaRepository.findById(metaId).orElse(null);
+
+        if(result == null)                                  throw new MetaNaoEncontradaException();
+        if(!result.getUsuario().getId().equals(usuarioId))  throw new AcessoNegadoException();
+
+        return new MetaDTO(result);
     }
 
-    @Transactional(readOnly = true)
-    public Meta save(Meta meta) {
-        return metaRepository.save(meta);
+    @Transactional
+    public MetaDTO save(String usuarioId, MetaPostDTO meta) {
+        Usuario usuario = usuarioRepository.findById(usuarioId).orElse(null);
+        if(usuario == null) throw new UsuarioNaoEncontradoException();
+
+        Meta metaEntity = new Meta(meta);
+        metaEntity.setUsuario(usuario);
+
+        return new MetaDTO(metaRepository.save(metaEntity));
     }
 
-    @Transactional(readOnly = true)
-    public Meta update(Meta meta) {
-        return metaRepository.save(meta);
+    @Transactional
+    public MetaDTO update(String usuarioId, Long id, MetaPostDTO meta) {
+        Meta metaEntity = metaRepository.findById(id).orElse(null);
+        if(metaEntity == null)                                  throw new MetaNaoEncontradaException();
+        if(!metaEntity.getUsuario().getId().equals(usuarioId))  throw new AcessoNegadoException();
+
+        metaEntity.setDescricao(meta.getDescricao());
+        metaEntity.setUrlImg(meta.getUrlImg());
+        metaEntity.setValorMeta(meta.getValorMeta());
+
+        return new MetaDTO(metaRepository.save(metaEntity));
     }
 
-    @Transactional(readOnly = true)
-    public void delete(String usuarioId, String metaId) {
-        Usuario usuario = usuarioRepository.findById(usuarioId).get();
+    @Transactional
+    public void delete(String usuarioId, Long id) {
+        Meta metaEntity = metaRepository.findById(id).orElse(null);
+
+        if(metaEntity == null)                                  throw new MetaNaoEncontradaException();
+        if(!metaEntity.getUsuario().getId().equals(usuarioId))  throw new AcessoNegadoException();
+
+        metaRepository.delete(metaEntity);
     }
 }
