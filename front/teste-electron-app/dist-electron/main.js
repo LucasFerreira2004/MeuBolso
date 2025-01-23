cd9cb2bfebab41f48f67fcfc9e4073966fae1c75
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-01-22 21:28:37 -0300

    front-config: alterando arquivos de configuração no package.json

--- front/teste-electron-app/dist-electron/main.js
+++ front/teste-electron-app/dist-electron/main.js
@@ -1,67 +1,32 @@
-import { app, BrowserWindow } from "electron";
-import { fileURLToPath } from "node:url";
-import path from "node:path";
-import { spawn } from "node:child_process";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-process.env.APP_ROOT = path.join(__dirname, "..");
-const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
-const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
-const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST;
-let win;
-let apiProcess;
-function startAPI() {
-  const apiPath = path.join(process.env.APP_ROOT, "api", "dslist.jar");
-  apiProcess = spawn("java", ["-jar", apiPath], { stdio: "inherit" });
-  apiProcess.on("error", (err) => {
-    console.error("Erro ao iniciar a API:", err);
-  });
-  apiProcess.on("exit", (code) => {
-    console.log("Processo da API foi encerrado com código:", code);
-  });
-}
-function stopAPI() {
-  if (apiProcess) {
-    apiProcess.kill();
-    apiProcess = null;
-  }
-}
-function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+import { app as n, BrowserWindow as i } from "electron";
+import { fileURLToPath as c } from "node:url";
+import o from "node:path";
+const t = o.dirname(c(import.meta.url));
+process.env.APP_ROOT = o.join(t, "..");
+const s = process.env.VITE_DEV_SERVER_URL, R = o.join(process.env.APP_ROOT, "dist-electron"), r = o.join(process.env.APP_ROOT, "dist");
+process.env.VITE_PUBLIC = s ? o.join(process.env.APP_ROOT, "public") : r;
+let e;
+function l() {
+  e = new i({
+    icon: o.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
-      preload: path.join(__dirname, "preload.mjs")
+      preload: o.join(t, "preload.mjs")
     }
-  });
-  win.webContents.on("did-finish-load", () => {
-    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
-  });
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL);
-  } else {
-    win.loadFile(path.join(RENDERER_DIST, "index.html"));
-  }
+  }), e.webContents.on("did-finish-load", () => {
+    e == null || e.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  }), s ? e.loadURL(s) : e.loadFile(o.join(r, "index.html"));
 }
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-    win = null;
-  }
+n.on("window-all-closed", () => {
+  process.platform !== "darwin" && (n.quit(), e = null);
 });
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+n.on("activate", () => {
+  i.getAllWindows().length === 0 && l();
 });
-app.on("before-quit", () => {
-  stopAPI();
-});
-app.whenReady().then(() => {
-  startAPI();
-  createWindow();
+n.whenReady().then(() => {
+  l();
 });
 export {
-  MAIN_DIST,
-  RENDERER_DIST,
-  VITE_DEV_SERVER_URL
+  R as MAIN_DIST,
+  r as RENDERER_DIST,
+  s as VITE_DEV_SERVER_URL
 };
--- front/teste-electron-app/electron/main.ts
+++ front/teste-electron-app/electron/main.ts
@@ -2,7 +2,7 @@ import { app, BrowserWindow } from 'electron'
 //import { createRequire } from 'node:module'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
-import { spawn } from 'node:child_process'
+
 
 //const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -17,28 +17,7 @@ export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
 process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
 
 let win: BrowserWindow | null
-let apiProcess: any // Referência para o processo da API
 
-function startAPI() {
-  const apiPath = path.join(process.env.APP_ROOT, 'api', 'dslist.jar')
-  
-  // Lança o .jar usando o Java instalado no sistema
-  apiProcess = spawn('java', ['-jar', apiPath], { stdio: 'inherit' })
-  apiProcess.on('error', (err: any) => { //verificar se isso tá certo, desse tipo any
-    console.error('Erro ao iniciar a API:', err)
-  })
-
-  apiProcess.on('exit', (code: any) => {
-    console.log('Processo da API foi encerrado com código:', code)
-  })
-}
-
-function stopAPI() {
-  if (apiProcess) {
-    apiProcess.kill() // Finaliza o processo da API ao fechar o app
-    apiProcess = null
-  }
-}
 
 function createWindow() {
   win = new BrowserWindow({
@@ -74,11 +53,8 @@ app.on('activate', () => {
   }
 })
 
-app.on('before-quit', () => {
-  stopAPI() // Certifica-se de parar a API antes de sair
-})
+
 
 app.whenReady().then(() => {
-  startAPI() // Inicia a API
   createWindow() // Cria a janela principal
 })
