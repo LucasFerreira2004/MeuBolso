0285b9a475c7aca683979a517d3dbd4703362484
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-01-28 14:14:01 -0300

    back-feat: add criarCategoriasService, responsável por criar as categorias padrão do sistema e do usuário

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/categoria/CriarCategoriasIniciaisService.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/categoria/CriarCategoriasIniciaisService.java
@@ -6,6 +6,9 @@ import com.projetointegrado.MeuBolso.usuario.UsuarioValidateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CriarCategoriasIniciaisService {
     @Autowired
@@ -15,10 +18,35 @@ public class CriarCategoriasIniciaisService {
     private UsuarioValidateService usuarioValidateService;
     public void criarCategorias(String userId) {
         Usuario usuario = usuarioValidateService.validateAndGet(userId, new EntidadeNaoEncontradaException("{token}", "usuario não encontrado"));
+        List<Categoria> categoriasPadrao = List.of(
+            criarCategoriaInterna(usuario, "DepositoInicial*", TipoCategoria.RECEITA),
+            criarCategoriaInterna(usuario, "ReajusteSaldoAumento*", TipoCategoria.RECEITA),
+            criarCategoriaInterna(usuario, "ReajusteSaldoDecremento*", TipoCategoria.DESPESA),
+
+            criarCategoriaExterna(usuario, "salario", TipoCategoria.RECEITA),
+            criarCategoriaExterna(usuario, "investimentos", TipoCategoria.RECEITA),
+            criarCategoriaExterna(usuario, "presente", TipoCategoria.RECEITA),
+            criarCategoriaExterna(usuario, "renda extra", TipoCategoria.RECEITA),
+            criarCategoriaExterna(usuario, "outros", TipoCategoria.RECEITA),
+
+            criarCategoriaExterna(usuario, "casa", TipoCategoria.DESPESA),
+            criarCategoriaExterna(usuario, "alimentacão", TipoCategoria.DESPESA),
+            criarCategoriaExterna(usuario, "transporte", TipoCategoria.DESPESA),
+            criarCategoriaExterna(usuario, "educacão", TipoCategoria.DESPESA),
+            criarCategoriaExterna(usuario, "lazer", TipoCategoria.DESPESA),
+            criarCategoriaExterna(usuario, "roupas", TipoCategoria.DESPESA),
+            criarCategoriaExterna(usuario, "saúde", TipoCategoria.DESPESA),
+            criarCategoriaExterna(usuario, "pagamentos", TipoCategoria.DESPESA)
+
+        );
 
     }
 
-    private Categoria criarCategoria(Usuario usuario, String nome, TipoCategoria tipo) {
+    private Categoria criarCategoriaInterna(Usuario usuario, String nome, TipoCategoria tipo) {
+        Categoria categoria = new Categoria(null, nome, tipo, "000", true, usuario, true);
+        return categoria;
+    }
+    private Categoria criarCategoriaExterna(Usuario usuario, String nome, TipoCategoria tipo) {
         Categoria categoria = new Categoria(null, nome, tipo, "000", true, usuario, true);
         return categoria;
     }
