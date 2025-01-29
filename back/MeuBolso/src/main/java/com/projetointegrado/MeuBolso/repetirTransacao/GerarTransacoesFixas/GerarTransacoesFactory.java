b91f86b2229c79096f5db0104c76349e092b3c7d
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-01-29 20:46:24 -0300

    back-feat: add geração de transações a partir de parceladas

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/repetirTransacao/GerarTransacoesFixas/GerarTransacoesFactory.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/repetirTransacao/GerarTransacoesFixas/GerarTransacoesFactory.java
@@ -2,14 +2,27 @@ package com.projetointegrado.MeuBolso.repetirTransacao.GerarTransacoesFixas;
 
 import com.projetointegrado.MeuBolso.repetirTransacao.TipoRepeticao;
 import com.projetointegrado.MeuBolso.transacao.TipoTransacao;
+import com.projetointegrado.MeuBolso.transacao.TransacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GerarTransacoesFactory {
-    public static IGerarTransacoesStrategy gerarTransacoesStrategy(TipoRepeticao tipoRepeticao) {
+    @Autowired
+    FixasGerarTransacoes fixasGerarTransacoes;
+
+    @Autowired
+    ParceladasGerarTransacoes parceladasGerarTransacoes;
+
+    public GerarTransacoesFactory() {}
+
+    public IGerarTransacoesStrategy gerarTransacoesStrategy(TipoRepeticao tipoRepeticao) {
+
         switch (tipoRepeticao) {
             case FIXO:
-                return new FixasGerarTransacoes();
+                return fixasGerarTransacoes;
             case PARCELAMENTO:
-                return new ParceladasGerarTransacoes();
+                return parceladasGerarTransacoes;
             default:
                 throw new IllegalArgumentException("Periodicidade desconhecida");
         }
