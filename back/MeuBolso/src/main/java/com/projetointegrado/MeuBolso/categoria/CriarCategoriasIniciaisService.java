85a3fdd8d78743f4abb0681ad52357a59b5a1c78
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-01-28 21:04:02 -0300

    back-refac: retornando apenas as transações de um único mês na rota de findAll

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/categoria/CriarCategoriasIniciaisService.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/categoria/CriarCategoriasIniciaisService.java
@@ -19,34 +19,34 @@ public class CriarCategoriasIniciaisService {
     public void criarCategorias(String userId) {
         Usuario usuario = usuarioValidateService.validateAndGet(userId, new EntidadeNaoEncontradaException("{token}", "usuario não encontrado"));
         List<Categoria> categoriasPadrao = List.of(
-            criarCategoriaInterna(usuario, "DepositoInicial*", TipoCategoria.RECEITA),
-            criarCategoriaInterna(usuario, "ReajusteSaldoAumento*", TipoCategoria.RECEITA),
-            criarCategoriaInterna(usuario, "ReajusteSaldoDecremento*", TipoCategoria.DESPESA),
+            criarCategoriaInterna(usuario, "DepositoInicial*", TipoCategoria.RECEITA, "000"),
+            criarCategoriaInterna(usuario, "ReajusteSaldoAumento*", TipoCategoria.RECEITA, "000"),
+            criarCategoriaInterna(usuario, "ReajusteSaldoDecremento*", TipoCategoria.DESPESA, "000"),
 
-            criarCategoriaExterna(usuario, "salario", TipoCategoria.RECEITA),
-            criarCategoriaExterna(usuario, "investimentos", TipoCategoria.RECEITA),
-            criarCategoriaExterna(usuario, "presente", TipoCategoria.RECEITA),
-            criarCategoriaExterna(usuario, "renda extra", TipoCategoria.RECEITA),
-            criarCategoriaExterna(usuario, "outros", TipoCategoria.RECEITA),
+            criarCategoriaExterna(usuario, "salário", TipoCategoria.RECEITA, "33A841"),
+            criarCategoriaExterna(usuario, "investimentos", TipoCategoria.RECEITA, "0775BA"),
+            criarCategoriaExterna(usuario, "presente", TipoCategoria.RECEITA, "8755BC"),
+            criarCategoriaExterna(usuario, "renda extra", TipoCategoria.RECEITA, "CE601B"),
+            criarCategoriaExterna(usuario, "outros", TipoCategoria.RECEITA, "BC7D18"),
 
-            criarCategoriaExterna(usuario, "casa", TipoCategoria.DESPESA),
-            criarCategoriaExterna(usuario, "alimentacão", TipoCategoria.DESPESA),
-            criarCategoriaExterna(usuario, "transporte", TipoCategoria.DESPESA),
-            criarCategoriaExterna(usuario, "educacão", TipoCategoria.DESPESA),
-            criarCategoriaExterna(usuario, "lazer", TipoCategoria.DESPESA),
-            criarCategoriaExterna(usuario, "roupas", TipoCategoria.DESPESA),
-            criarCategoriaExterna(usuario, "saúde", TipoCategoria.DESPESA),
-            criarCategoriaExterna(usuario, "pagamentos", TipoCategoria.DESPESA)
+            criarCategoriaExterna(usuario, "casa", TipoCategoria.DESPESA, "8755BC"),
+            criarCategoriaExterna(usuario, "alimentação", TipoCategoria.DESPESA, "BA0707"),
+            criarCategoriaExterna(usuario, "transporte", TipoCategoria.DESPESA,"C19F13"),
+            criarCategoriaExterna(usuario, "educação", TipoCategoria.DESPESA, "33A841"),
+            criarCategoriaExterna(usuario, "lazer", TipoCategoria.DESPESA, "57BEA1"),
+            criarCategoriaExterna(usuario, "roupas", TipoCategoria.DESPESA, "5797BE"),
+            criarCategoriaExterna(usuario, "saúde", TipoCategoria.DESPESA, "C92D78"),
+            criarCategoriaExterna(usuario, "pagamentos", TipoCategoria.DESPESA, "818181n")
         );
         categoriaRepository.saveAll(categoriasPadrao);
     }
 
-    private Categoria criarCategoriaInterna(Usuario usuario, String nome, TipoCategoria tipo) {
-        Categoria categoria = new Categoria(null, nome, tipo, "000", true, usuario, true);
+    private Categoria criarCategoriaInterna(Usuario usuario, String nome, TipoCategoria tipo, String cor) {
+        Categoria categoria = new Categoria(null, nome, tipo, cor, true, usuario, true);
         return categoria;
     }
-    private Categoria criarCategoriaExterna(Usuario usuario, String nome, TipoCategoria tipo) {
-        Categoria categoria = new Categoria(null, nome, tipo, "000", true, usuario, false);
+    private Categoria criarCategoriaExterna(Usuario usuario, String nome, TipoCategoria tipo, String cor) {
+        Categoria categoria = new Categoria(null, nome, tipo, cor, true, usuario, false);
         return categoria;
     }
 }
