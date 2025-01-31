6d2d1938302a5cef208b3ccd10a6913b97c3f86b
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-01-31 01:14:58 -0300

    back-refac: separando codigo em categoriaDadosService em funcoes

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/dashBoards/DashBoardController.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/dashBoards/DashBoardController.java
@@ -1,14 +1,12 @@
 package com.projetointegrado.MeuBolso.dashBoards;
 
+import com.projetointegrado.MeuBolso.dashBoards.dto.CategoriaExpandedDTO;
 import com.projetointegrado.MeuBolso.dashBoards.dto.CategoriaMinDTO;
 import com.projetointegrado.MeuBolso.transacao.TipoTransacao;
 import com.projetointegrado.MeuBolso.usuario.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -35,12 +33,13 @@ public class DashBoardController {
 
     @GetMapping("/receitasCategoria")
     public List<CategoriaMinDTO> getReceitasCategorias(@RequestParam("data") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data) {
-        try {
-            String userId = usuarioService.getUsuarioLogadoId();
-            return categoriaDashboardService.findAllValorTotalCategoria(userId, data, TipoTransacao.RECEITA);
-        }catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+        String userId = usuarioService.getUsuarioLogadoId();
+        return categoriaDashboardService.findAllValorTotalCategoria(userId, data, TipoTransacao.RECEITA);
+    }
+
+    @GetMapping("/categoria/{id}")
+    public CategoriaExpandedDTO getExpandedCategoria(@PathVariable Long id, @RequestParam("data") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data){
+        String userId = usuarioService.getUsuarioLogadoId();
+        return categoriaDashboardService.findExpandedCategoria(userId, id, data);
     }
 }
