8e4f0c53e9095656c31359cfd12227e4b396f566
Lucas Ferreira <lukasnoobre53@gmail.com>
2025-02-12 23:28:12 -0300

    back-config: adicionando chaves de acesso a api no .env

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/MeuBolsoApplication.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/MeuBolsoApplication.java
@@ -2,7 +2,11 @@ package com.projetointegrado.MeuBolso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.cloudinary.*;
+import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
 
+import java.util.Map;
 @SpringBootApplication
 public class MeuBolsoApplication {
 
