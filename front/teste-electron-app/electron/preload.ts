ac76c7259851e23a07342c4f87c22fce18640758
RewelliOliveira <rewelli@alu.ufc.br>
2025-02-18 19:47:34 -0300

    front-fix: preload

--- front/teste-electron-app/electron/preload.ts
+++ front/teste-electron-app/electron/preload.ts
@@ -1,24 +1,52 @@
-import { ipcRenderer, contextBridge } from 'electron'
+import { ipcRenderer, contextBridge, IpcRendererEvent } from 'electron';
 
-// --------- Expose some API to the Renderer process ---------
+// Tipos para os métodos expostos
+type Listener<T = unknown> = (event: IpcRendererEvent, ...args: T[]) => void;
+
+// Expõe APIs seguras para o Renderer Process
 contextBridge.exposeInMainWorld('ipcRenderer', {
-  on(...args: Parameters<typeof ipcRenderer.on>) {
-    const [channel, listener] = args
-    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
-  },
-  off(...args: Parameters<typeof ipcRenderer.off>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.off(channel, ...omit)
-  },
-  send(...args: Parameters<typeof ipcRenderer.send>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.send(channel, ...omit)
-  },
-  invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.invoke(channel, ...omit)
+  /**
+   * Escuta eventos enviados do Main Process.
+   * @param channel - Nome do canal.
+   * @param listener - Função de callback que será chamada quando o evento for recebido.
+   */
+  on<T = unknown>(channel: string, listener: Listener<T>): void {
+    ipcRenderer.on(channel, listener);
   },
 
-  // You can expose other APTs you need here.
-  // ...
-})
+  /**
+   * Remove um listener específico de um canal.
+   * @param channel - Nome do canal.
+   * @param listener - Função de callback a ser removida.
+   */
+  off<T = unknown>(channel: string, listener: Listener<T>): void {
+    ipcRenderer.off(channel, listener);
+  },
+
+  /**
+   * Envia uma mensagem assíncrona para o Main Process.
+   * @param channel - Nome do canal.
+   * @param args - Argumentos a serem enviados.
+   */
+  send<T = unknown>(channel: string, ...args: T[]): void {
+    ipcRenderer.send(channel, ...args);
+  },
+
+  /**
+   * Envia uma mensagem assíncrona para o Main Process e aguarda uma resposta.
+   * @param channel - Nome do canal.
+   * @param args - Argumentos a serem enviados.
+   * @returns Uma Promise que resolve com a resposta do Main Process.
+   */
+  invoke<T = unknown, R = unknown>(channel: string, ...args: T[]): Promise<R> {
+    return ipcRenderer.invoke(channel, ...args);
+  },
+
+  /**
+   * Remove todos os listeners de um canal específico.
+   * @param channel - Nome do canal.
+   */
+  removeAllListeners(channel: string): void {
+    ipcRenderer.removeAllListeners(channel);
+  },
+});
