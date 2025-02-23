c85ac752d297b93ca6a17a5077beee9c4acd2c56
RewelliOliveira <rewelli@alu.ufc.br>
2025-02-23 00:28:34 -0300

    front-feat: tela de perfil

---
+++ front/teste-electron-app/src/pages/Perfil/perfil.tsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import style from "./perfil.module.css";
+import BalancoBancos from "../../components/UI/ChartsRelatorios/BalancoBancos/balanco-bancos";
+
+function Perfil() {
+  const [usuario, setUsuario] = useState({
+    nome: "",
+    email: "",
+    imgUrl: null,
+  });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchUsuario = async () => {
+    const token = localStorage.getItem("authToken");
+    if (!token) {
+      setError("Você precisa estar logado para realizar esta ação");
+      setLoading(false);
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const response = await axios.get("http://localhost:8080/usuarios", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setUsuario(response.data);
+    } catch (error: any) {
+      console.error("Erro ao buscar dados do usuário:", error);
+
+      if (error.response) {
+        console.error("Resposta do servidor:", error.response.data);
+        console.error("Status do erro:", error.response.status);
+      }
+
+      setError("Erro ao carregar dados do usuário");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsuario();
+  }, []);
+
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className={style.containerPerfil}>
+      <header className={style.headerPerfil}>
+        <h1>Perfil</h1>
+      </header>
+      <div className={style.body}>
+        <div className={style.headerPerfil}>
+          <div className={style.fotoPerfil}>
+            {usuario.imgUrl ? (
+              <img src={usuario.imgUrl} alt="Foto do perfil" />
+            ) : (
+              "Foto aqui"
+            )}
+          </div>
+          <div className={style.infoPerfil}>
+            <div className={style.infoItem}>
+              <strong>Nome:</strong>
+              <span>{usuario.nome}</span>
+            </div>
+            <div className={style.infoItem}>
+              <strong>Email:</strong>
+              <span>{usuario.email}</span>
+            </div>
+          </div>
+        </div>
+        <div className={style.classButton}>
+          <button className={style.button}>
+            <img src="/assets/iconsPerfil/editperfil.svg" alt="" />
+            Editar informações
+          </button>
+        </div>
+      </div>
+      <BalancoBancos/>
+    </div>
+  );
+}
+
+export default Perfil;
