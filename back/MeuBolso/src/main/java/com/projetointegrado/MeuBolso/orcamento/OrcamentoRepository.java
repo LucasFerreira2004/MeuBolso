53d71f77c5da53d5dbab3474ff330e43c8988dbc
Miqueias Bento <miqueias.bento.da.silva@gmail.com>
2025-02-27 10:35:22 -0300

    back-refac: refatorando consultas em Orcamento

--- back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/orcamento/OrcamentoRepository.java
+++ back/MeuBolso/src/main/java/com/projetointegrado/MeuBolso/orcamento/OrcamentoRepository.java
@@ -11,13 +11,29 @@ public interface OrcamentoRepository extends JpaRepository<Orcamento, Long> {
     @Query("SELECT o FROM Orcamento o WHERE o.usuario.id = :idUsuario")
     List<Orcamento> findAllByUsuario(String idUsuario);
 
-    @Query(value = "SELECT * FROM orcamento o WHERE o.usuario_id = :usuarioId AND o.ano = :ano AND o.mes = :mes AND o.categoria_id = :categoriaId", nativeQuery = true)
+    // Busca um orçamento específico para um usuário, em um determinado período e categoria.
+    @Query(value = """
+    SELECT o.* 
+    FROM orcamento o
+    JOIN categoria c ON o.categoria_id = c.id
+    WHERE o.usuario_id = :usuarioId 
+      AND o.ano = :ano 
+      AND o.mes = :mes 
+      AND c.id = :categoriaId
+    """, nativeQuery = true)
     Optional<Orcamento> findByCategoriaUsuarioAndPeriodo(@Param("categoriaId") Long categoriaId,
                                                          @Param("usuarioId") String usuarioId,
                                                          @Param("ano") int ano,
                                                          @Param("mes") int mes);
 
-    @Query(value = "SELECT * FROM orcamento o WHERE o.usuario_id = :usuarioId AND o.ano = :ano AND o.mes = :mes", nativeQuery = true)
+    // Busca todos os orçamentos de um usuário para um determinado período.
+    @Query(value = """
+    SELECT o.* 
+    FROM orcamento o
+    WHERE o.usuario_id = :usuarioId 
+      AND o.ano = :ano 
+      AND o.mes = :mes
+    """, nativeQuery = true)
     List<Orcamento> findByUsuarioAndPeriodo(@Param("usuarioId") String usuarioId,
                                             @Param("ano") int ano,
                                             @Param("mes") int mes);
